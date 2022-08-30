ef35632706c45169cabc2cefeb49d51ef0f323e0
mmarcato <marinaramarcato@gmail.com>
2022-08-30 12:46:10 +0100

    Fixed PRQ RFE issues + added performance metrics

--- source/prq_main.py
+++ source/prq_main.py
@@ -75,38 +75,38 @@ def score(df):
                                 np.nan , df.mean(axis = 1))
 
 def factor(df):
-    df['F_01 Stranger-directed aggression'] = score(df.iloc[:,[10, 11, 12, 15, 16, 18, 20, 21, 22, 28]] )
-    df['F_02 Owner-directed aggression'] = score(df.iloc[:,[9, 13, 14, 17, 19, 25, 30, 31]] )
-    df['F_03 Dog-directed aggression'] = score(df.iloc[:,[23, 24, 26, 29]] )
-    df['F_04 Dog-directed fear'] = score(df.iloc[:,[45, 46, 52, 53]] )
-    df['F_05 Dog rivalry'] = score(df.iloc[:,[32, 33, 34, 35]] )
-    df['F_06 Trainability'] = score(df.iloc[:,[1, 2, 3, 4, 5, 6, 7, 8]] )
-    df['F_07 Chasing'] = score(df.iloc[:,[27, 74, 75, 76]] )
-    df['F_08 Stranger-directed fear'] = score(df.iloc[:,[36, 37, 39, 40]] )
-    df['F_09 Nonsocial fear'] = score(df.iloc[:,[38, 41, 42, 44, 47, 48]] )
-    df['F_10 Separation-related problems'] = score(df.iloc[:,[54, 55, 56, 57, 58, 59, 60, 61]] ) 
-    df['F_11 Touch sensitivity'] = score(df.iloc[:,[43, 49, 50, 51]] ) 
-    df['F_12 Excitability'] = score(df.iloc[:,[62, 63, 64, 65, 66, 67]] ) 
-    df['F_13 Attachment/attention-seeking'] = score(df.iloc[:,[68, 69, 70, 71, 72, 73]] ) 
+    df['F_01 Trainability'] = score(df.iloc[:,[1, 2, 3, 4, 5, 6, 7, 8]] )
+    df['F_02 Stranger-directed aggression'] = score(df.iloc[:,[10, 11, 12, 15, 16, 18, 20, 21, 22, 28]] )
+    df['F_03 Owner-directed aggression'] = score(df.iloc[:,[9, 13, 14, 17, 19, 25, 30, 31]] )
+    df['F_04 Dog rivalry'] = score(df.iloc[:,[32, 33, 34, 35]] )
+    df['F_05 Stranger-directed fear'] = score(df.iloc[:,[36, 37, 39, 40]] )
+    df['F_06 Nonsocial fear'] = score(df.iloc[:,[38, 41, 42, 44, 47, 48]] )
+    df['F_07 Dog-directed aggression'] = score(df.iloc[:,[23, 24, 26, 29]] )
+    df['F_08 Dog-directed fear'] = score(df.iloc[:,[45, 46, 52, 53]] )
+    df['F_09 Touch sensitivity'] = score(df.iloc[:,[43, 49, 50, 51]] ) 
+    df['F_10 Separation-related behaviour'] = score(df.iloc[:,[54, 55, 56, 57, 58, 59, 60, 61]] ) 
+    df['F_11 Excitability'] = score(df.iloc[:,[62, 63, 64, 65, 66, 67]] ) 
+    df['F_12 Attachment/attention-seeking'] = score(df.iloc[:,[68, 69, 70, 71, 72, 73]] ) 
+    df['F_13 Chasing'] = score(df.iloc[:,[27, 74, 75, 76]] )
     df['F_14 Energy'] = score(df.iloc[:,[91, 92]] ) 
 
     return df
 
 ''' Formulas for calculating factors
     The C-BARQ provides a set of quantitative scores for the following fourteen different subscales or categories of behavior:
+        'Trainability' score = (items 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)/8—remember to reverse scoring order for items 5, 6 & 7 (see above).
         'Stranger-directed aggression' score = (questionnaire items 10 + 11 + 12 + 15 + 16 + 18 + 20 + 21 + 22 + 28)/10.
         'Owner-directed aggression' score = (items 9 + 13 + 14 + 17 + 19 + 25 + 30 + 31)/8.
-        'Dog-directed aggression' = (items 23 + 24 + 26 + 29)/4
-        'Dog-directed fear' = (items 45 + 46 + 52 + 53)/4.
         'Dog rivalry'(familiar dog aggression) score = (items 32 + 33 + 34 + 35)/4
-        'Trainability' score = (items 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)/8—remember to reverse scoring order for items 5, 6 & 7 (see above).
-        'Chasing' score = (items 27 + 74 + 75 + 76)/4
         'Stranger-directed fear' score = (items 36 + 37 + 39 + 40)/4
         'Nonsocial fear' score = (items 38 + 41 + 42 + 44 + 47 + 48)/6
-        'Separation-related problems' score = (items 54 + 55 + 56 + 57 + 58 + 59 + 60 + 61)/8
+        'Dog-directed aggression' = (items 23 + 24 + 26 + 29)/4
+        'Dog-directed fear' = (items 45 + 46 + 52 + 53)/4.
         'Touch sensitivity' score = (items 43 + 49 + 50 + 51)/4
+        'Separation-related problems' score = (items 54 + 55 + 56 + 57 + 58 + 59 + 60 + 61)/8
         'Excitability' score = (items 62 + 63 + 64 + 65 + 66 + 67)/6
         'Attachment/attention-seeking' score = (items  68 + 69 + 70 + 71 + 72 + 73)/6
+        'Chasing' score = (items 27 + 74 + 75 + 76)/4
         'Energy' score = (items 91 + 92)/2
 
     If more than 25% of the items in a subscale are missing values, the factor/subscale score should be recorded as a missing value.
